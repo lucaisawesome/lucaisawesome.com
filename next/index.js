b8a028259b91a17f39ef8635a93f887780c674c3
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2020-11-01 14:20:12 -0800

    feat(next): improve responsiveness a bit

--- next/index.js
+++ next/index.js
@@ -1,1 +1,16 @@
-// TODO
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.documentElement;
+  const nose = document.querySelector(".nose");
+
+  nose.addEventListener("click", () => {
+    const currentHeight = window
+      .getComputedStyle(root)
+      .getPropertyValue("--hair-height")
+      .trim();
+
+    document.documentElement.style.setProperty(
+      "--hair-height",
+      `${parseInt(currentHeight) + 5}vh`
+    );
+  });
+});
